8959298eef8688e0f01f3805f1ba0dba69ff1837
xzz <3051717720@qq.com>
2016-07-19 11:53:36 +0800

    Initial Commit

---
+++ 语音demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  语音demo
+//
+//  Created by angelwin on 16/7/19.
+//  Copyright © 2016年 com@angelwin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
